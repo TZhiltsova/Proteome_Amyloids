d6bdc423f94ddd39c5c318bd1844705ae74c67bf
TZhiltsova <t.y.zhiltsova@mail.ru>
2023-02-05 13:33:03 +0300

    Adding functional files and full code without function

---
+++ README.md
@@ -0,0 +1,19 @@
+# Algorithm for searching of short amyloid motifs from Waltz-DB in Human Proteome
+
+Material for master's degree work in ITMO University.
+
+Student: Zhiltsova T.
+
+Supervisor: Kajava A., Pyankov I.
+
+## Abstract
+
+## Results
+
+## User Guid
+
+### Launch
+
+
+## References
+
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pyfaidx import Fasta
 import csv
 
-# reading of the file with amiloids motifs
-waltz_seq = []       # list for short motifs of amiloid
-waltz_dict = {}      # dictionry for sequences from WaltzDB
-with open('WALTZ_DB_amiloid_seq') as waltz_db:
+# reading of the file with amyloids motifs
+waltz_seq = []       # list for short motifs of amyloid
+waltz_dict = {}      # dictionary for sequences from WaltzDB
+with open('WALTZ_DB_amyloid_seq') as waltz_db:
     for line in waltz_db:
         line = line.strip()
         waltz_seq.append(line)
@@ -20,29 +20,22 @@ with Fasta('uniprot-proteome.fasta') as genes:
     for number_of_peptide in range((len(genes.keys()))):
         proteome_seq[genes[number_of_peptide][:].name] = genes[number_of_peptide][:]
 
-'''
-# reading tsv file, which contains UniProt names for all peptides from DisProt. They will be used in final fasta
-tsv_list = {}  # dict for pairs "UniProt_name - Sequence"
-with open('DisProt release_2022_06.tsv') as tsv:
-    file = csv.reader(tsv, delimiter='\t')
-    for rows in file:
-        while '' in rows:     # deleting empty elements from tsv
-            rows.remove('')
-        tsv_list[rows[len(rows)-1]] = rows[0]
-'''
-
-# finding peptides with amiloid motifs
-amiloid_motifs_list_for_header = {}  # remembering amiloid motifs for each peptide, which have amiloid-forming region
-filter_seq = {}  # dict for seq that have amiloid-forming regions
+# finding peptides with amyloid motifs
+new_csv = []
+amyloid_motifs_list_for_header = {}  # remembering amyloid motifs for each peptide, which have amyloid-forming region
+filter_seq = {}  # dict for seq that have amyloid-forming regions
 for key, seq in proteome_seq.items():
-    amiloid_motifs_list = []   # updated list for all amiloid motifs in one peptide
+    amyloid_motifs_list = []   # updated list for all amyloid motifs in one peptide
     for val in waltz_dict.values():
-        if val in str(seq):  #
+        if val in str(seq):
             filter_seq[key] = seq
             pos = str(seq).find(val)+1
             val = str(pos) + val
-            amiloid_motifs_list.append(val)
-        amiloid_motifs_list_for_header[key] = amiloid_motifs_list
+            amyloid_motifs_list.append(val)
+            name = key.split('|')
+            if name[1] not in new_csv:
+                new_csv.append(name[1])
+        amyloid_motifs_list_for_header[key] = amyloid_motifs_list
 
 discriptor = []
 with Fasta('uniprot-proteome.fasta') as genes:
@@ -50,17 +43,17 @@ with Fasta('uniprot-proteome.fasta') as genes:
         line = record.long_name
         discriptor.append(str(line))
 
-amiloid_seq_no_amil_disc = {}
+amyloid_seq_no_amil_disc = {}
 for key, val in filter_seq.items():
     for disc in discriptor:
         if key in disc:
-            amiloid_seq_no_amil_disc[disc] = val
+            amyloid_seq_no_amil_disc[disc] = val
 
 amil_seq = {}
 fasta_uniprot = {}
-for key, val in amiloid_seq_no_amil_disc.items():
+for key, val in amyloid_seq_no_amil_disc.items():
     head_for_fasta = str(key)
-    for key_amil, val_amil in amiloid_motifs_list_for_header.items():
+    for key_amil, val_amil in amyloid_motifs_list_for_header.items():
         if key_amil in key:
             for i in val_amil:
                 if ' amyloid_seq=' not in head_for_fasta:
@@ -70,33 +63,48 @@ for key, val in amiloid_seq_no_amil_disc.items():
     amil_seq[head_for_fasta] = val
 
 table = []
-with open('DisProt release_2022_06.tsv') as tsv:
+with open('uniprot-proteome.tsv') as tsv:
     file = csv.reader(tsv, delimiter='\t')
     count = 0
     for rows in file:
         if count == 0:
             table.append(rows)
             count += 1
-        for pep, uni in new_tsv.items():
-            if uni in rows and pep in rows:
+        for code in new_csv:
+            if code in rows:
                 table.append(rows)
 
-'''
-with open('table_amiloids.tsv', 'w') as tab:
-    for elem in table:
-        for words in elem:
-            tab.write(words + ', ')
-        tab.write('\n')
-'''
+table_short = []
+t = 1
+for line in table:
+    entry = line.index('Entry')
+    entry_name = line.index('Entry Name')
+    reviewed = line.index('Reviewed')
+    protein_names = line.index('Protein names')
+    gene_names = line.index('Gene Names')
+    organism = line.index('Organism')
+    length = line.index('Length')
+    break
 
-with open('Amyloid+Disprot_full_corr', 'w') as A_D:
+for elem in table:
+    table_short_words = []
+    table_short_words.append(elem[entry])
+    table_short_words.append(elem[entry_name])
+    table_short_words.append(elem[reviewed])
+    table_short_words.append(elem[protein_names])
+    table_short_words.append(elem[gene_names])
+    table_short_words.append(elem[organism])
+    table_short_words.append(elem[length])
+    table_short.append(table_short_words)
+
+with open('Amyloid_Proteome', 'w') as am_seq:
     for key, val in amil_seq.items():
-        A_D.write(str(key) + '\n')
-        A_D.write(str(val) + '\n')
+        am_seq.write(str(key) + '\n')
+        am_seq.write(str(val) + '\n')
 
 
-with open('UniProt+DisProt_amyloids.tsv', 'w') as tab:
+with open('table_prot_amyl.tsv', 'w') as tab:
     for elem in table_short:
         for words in elem:
-            tab.write(words + ', ')
+            tab.write(words + '\t')
         tab.write('\n')
